81cf6da91a2d67a9b02ae1e6970809d4e4f780e4
Yeferson Andres Velazco Galvis <92173102+YeanVega@users.noreply.github.com>
2021-11-27 23:51:05 -0500

    Add files via upload
    Intento 6

--- src/main/java/Fabricante.java
+++ src/main/java/Fabricante.java
@@ -1,12 +1,17 @@
 package vehiculos;
+import java.util.ArrayList;
 
 public class Fabricante {
 	private String nombre;
 	private Pais pais;
+	private static ArrayList<Fabricante> fabricantes = new ArrayList<Fabricante>();
+	private int contador;
 	
 	public Fabricante(String nombre, Pais pais) {
 		this.nombre = nombre;
 		this.pais = pais;
+		contador = 0;
+		fabricantes.add(this);
 	}
 
 	public String getNombre() {
@@ -23,6 +28,28 @@ public class Fabricante {
 
 	public void setPais(Pais pais) {
 		this.pais = pais;
+	}
+
+	public ArrayList<Fabricante> getFabricantes() {
+		return fabricantes;
+	}
+
+	public int getContador() {
+		return contador;
+	}
+
+	public void setContador() {
+		contador++;
+	}
+
+	public static Fabricante fabricaMayorVentas() {
+		Fabricante max = fabricantes.get(0);
+		for(int i = 1; i < fabricantes.size(); i++) {
+			if(fabricantes.get(i).getContador() > max.getContador()) {
+				max = fabricantes.get(i);
+			}
+		}
+		return max;
 	}
 	
 	
