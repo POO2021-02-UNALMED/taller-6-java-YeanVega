ca578c778741ed9fb09b667465070ba793d68d2b
Yeferson Andres Velazco Galvis <92173102+YeanVega@users.noreply.github.com>
2021-11-27 23:23:07 -0500

    Add files via upload

---
+++ src/main/java/Automovil.java
@@ -0,0 +1,27 @@
+package vehiculos;
+
+public class Automovil extends Vehiculo {
+	private int puestos;
+	private static int vehiculos = 0;
+
+
+	public Automovil(int placa, String nombre, int precio, float peso, Fabricante fabricante, int puestos) {
+		super(placa, 4, 100, nombre, precio, peso, "FWD", fabricante);
+		this.puestos = puestos;
+		vehiculos++;
+
+	}
+
+	public int getPuestos() {
+		return puestos;
+	}
+
+	public void setPuestos(int puestos) {
+		this.puestos = puestos;
+	}
+	
+	public static int getVehiculos() {
+		return vehiculos;
+	}
+	
+}
---
+++ src/main/java/Vehiculo.java
@@ -0,0 +1,94 @@
+package vehiculos;
+
+public class Vehiculo {
+	protected int placa;
+	protected int puertas;
+	protected int velocidadMaxima;
+	protected String nombre;
+	protected int precio;
+	protected float peso;
+	protected String traccion;
+	protected Fabricante fabricante;
+	private static int vehiculos = 0;
+	
+	public Vehiculo(int placa, int puertas, int velocidadMaxima, String nombre, int precio, float peso, String traccion, Fabricante fabricante) {
+		this.placa = placa;
+		this.puertas = puertas;
+		this.velocidadMaxima = velocidadMaxima;
+		this.nombre = nombre;
+		this.precio = precio;
+		this.peso = peso;
+		this.traccion = traccion;
+		this.fabricante = fabricante;
+		vehiculos ++;
+	}
+	
+	public int getPlaca() {
+		return placa;
+	}
+	
+	public void setPlaca(int placa) {
+		this.placa = placa;
+	}
+	
+	public int getPuertas() {
+		return puertas;
+	}
+	
+	public void setPuertas(int puertas) {
+		this.puertas = puertas;
+	}
+	
+	public int getVelocidadMaxima() {
+		return velocidadMaxima;
+	}
+	public void setVelocidadMaxima(int velocidadMaxima) {
+		this.velocidadMaxima = velocidadMaxima;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public int getPrecio() {
+		return precio;
+	}
+	
+	public void setPrecio(int precio) {
+		this.precio = precio;
+	}
+	
+	public float getPeso() {
+		return peso;
+	}
+	
+	public void setPeso(float peso) {
+		this.peso = peso;
+	}
+	
+	public String getTraccion() {
+		return traccion;
+	}
+	
+	public void setTraccion(String traccion) {
+		this.traccion = traccion;
+	}
+	
+	public Fabricante getFabricante() {
+		return fabricante;
+	}
+	
+	public void setFabricante(Fabricante fabricante) {
+		this.fabricante = fabricante;
+	}
+	
+	
+	
+	
+	
+	
+}
